1908401f14c2f17a5f22921282ed43439abf1fd3
Spimy <officialspyme@gmail.com>
2020-07-05 18:06:53 +0400

    Add more prefixes

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const config = {
-    prefixes: ['>'] as string[],
+    prefixes: ['>', 'amadeus ', '<@&729317627482079262> '] as string[],
     memberRole: '424186949163548672' as string,
     muteRole: '579687632150331413' as string
 }
