4fae94d9813ef52c6a94c25c9e9df44489d3b414
Spimy <officialspyme@gmail.com>
2020-07-05 17:25:09 +0400

    Add optional string type

--- src/types.ts
+++ src/types.ts
@@ -3,8 +3,10 @@ import { Music } from './lib/music/Music';
 
 type EventType = keyof ClientEvents;
 type Pages = Music[][];
+type OptionalStr = string | undefined;
 
 export {
     EventType,
-    Pages
+    Pages,
+    OptionalStr
 }
